c6316606723eff3e5fa7466c96dcd33279100aa9
Maciej Laskowski <19705838+malaskowski@users.noreply.github.com>
2023-11-20 14:38:11 +0100

    fix jobs and tests names

--- websight-cms/test-scripts/login-with-invalid-password.sh
+++ websight-cms/test-scripts/login-with-invalid-password.sh
@@ -2,6 +2,7 @@
 
 set -x
 
+# Authenticate with invalid password - request should return 401
 curl -s -o /dev/null -w "%{http_code}" http://websight-cms:8080/apps/websight-authentication/j_security_check \
   --data-raw '_charset_=UTF-8&resource=%2F&j_username=wsadmin&j_password=wsadmin' \
   -ipv4 | grep '401'
